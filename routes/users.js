eb807d3e092d48f546274062a8d2ef32b3c03053
mylovetop <zdsoft@zdsoftmatoMac-mini-2.local>
2014-07-04 17:37:02 +0800

    添加了mysql 连接池
    添加了mysql 连接池

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,35 @@
-var express = require('express');
-var router = express.Router();
+var express         = require('express');
+var router          = express.Router();
+var path            = require('path');
+//var db              = require(path.resolve('./lib/data/db'));
+var pool            = require(path.resolve('./lib/data/pool'));
 
 /* GET users listing. */
 router.get('/', function(req, res) {
 //  res.send('respond with a resource');
   console.log('req.query.user=' + req.query.user);
 
-  var list = ['123', '3333', '4444', '这是什么'];
-  res.render('user',{
-    user:
-    {
-      name:req.query.user
-    },
-    list:list
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * FROM youyiku.user_info', function(err, rows, fields) {
+      console.log('mysql 连接开始');
+      if (err) throw err;
+      var list = ['123', '3333', '4444', '这是什么'];
+      res.render('user',{
+        user:
+        {
+          name:req.query.user
+        },
+        list:list
+      });
+      console.log('The solution is: ', rows[0].solution);
+      console.log('The solution is: ', fields);
+    });
+
+    connection.release();
+
   });
+
+
 });
 
 module.exports = router;
