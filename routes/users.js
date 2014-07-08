d52f280bdaed3e54663c0f87ce603bb4e22b1aeb
mylovetop <zdsoft@zdsoftmatoMac-mini-2.local>
2014-07-08 18:10:24 +0800

    修改了连接数据库方式
    修改了连接数据库方式

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,39 @@ var path            = require('path');
 //var db              = require(path.resolve('./lib/data/db'));
 var pool            = require(path.resolve('./lib/data/pool'));
 
+var userDao         = require(path.resolve('./lib/dao/user-dao'));
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-//  res.send('respond with a resource');
-  console.log('req.query.user=' + req.query.user);
 
-  pool.getConnection(function(err, connection){
-    connection.query('SELECT * FROM youyiku.user_info', function(err, rows, fields) {
-      console.log('mysql 连接开始');
-      if (err) throw err;
-//      var list = ['123', '3333', '4444', '这是什么'];
-      var list = rows;
+
+//  res.send('respond with a resource');
+//  console.log('req.query.user=' + req.query.user);
+//  pool.getConnection(function(err, connection){
+//    connection.query('SELECT * FROM youyiku.user_info', function(err, rows, fields) {
+//      console.log('mysql 连接开始');
+//      if (err) throw err;
+////      var list = ['123', '3333', '4444', '这是什么'];
+//      var list = rows;
+//      res.render('user',{
+//        user:
+//        {
+//          name:req.query.user
+//        },
+//        list:list
+//      });
+//
+//      console.log('The solution is: ', rows);
+//
+//    });
+//
+//    connection.release();
+//
+//  });
+
+  function callback(rows){
+
+    var list = rows;
       res.render('user',{
         user:
         {
@@ -22,16 +44,9 @@ router.get('/', function(req, res) {
         },
         list:list
       });
+  }
 
-      console.log('The solution is: ', rows);
-      console.log('The solution is: ', rows[0]);
-      console.log('The solution is: ', fields);
-    });
-
-    connection.release();
-
-  });
-
+  (new userDao()).queryUserInfo(callback);
 
 });
 
